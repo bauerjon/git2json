26f73dd6a9717662627b86e04b8b2c5d6a3407a5
Jon Bauer <bauerjon@hotmail.com>
2020-04-12 17:59:04 -0400

    adds ability to execute directly

---
+++ run.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Generate a json log of a git repository.
+"""
+
+from __future__ import print_function
+import json
+import sys
+from git2json.parser import parse_commits
+
+__author__ = 'Tavish Armstrong'
+__email__ = 'tavisharmstrong@gmail.com'
+__version__ = '0.2.3'
+
+# -------------------------------------------------------------------
+# Main
+
+
+def main():
+    print('got here')
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--git-dir',
+        default=None,
+        help='Path to the .git/ directory of the repository you are targeting'
+    )
+    parser.add_argument(
+        '--since',
+        default=None,
+        help=('Show commits more recent than a specific date. If present, '
+              'this argument is passed through to "git log" unchecked. ')
+    )
+    parser.add_argument(
+        '--until',
+        default=None,
+        help=('Show commits before a specific date. If present, '
+              'this argument is passed through to "git log" unchecked. ')
+    )
+    args = parser.parse_args()
+    if sys.version_info < (3, 0):
+        print(git2json(run_git_log(args.git_dir, args.since, args.until)))
+    else:
+        print(git2jsons(run_git_log(args.git_dir, args.since, args.until)))
+
+# -------------------------------------------------------------------
+# Main API functions
+
+def git2jsons(s):
+    return json.dumps(list(parse_commits(s)), ensure_ascii=False)
+
+
+def git2json(fil):
+    return json.dumps(list(parse_commits(fil.read())), ensure_ascii=False)
+
+
+# -------------------------------------------------------------------
+# Functions for interfacing with git
+
+
+def run_git_log(git_dir=None, git_since=None, git_until=None):
+    '''run_git_log([git_dir]) -> File
+
+    Run `git log --numstat --pretty=raw --remotes on the specified
+    git repository and return its stdout as a pseudo-File.'''
+    import subprocess
+    if git_dir:
+        command = [
+            'git',
+            '--git-dir=' + git_dir,
+            'log',
+            '--numstat',
+            '--pretty=raw',
+            '--remotes'
+        ]
+    else:
+        command = ['git', 'log', '--numstat', '--pretty=raw', '--remotes']
+    if git_since is not None:
+        command.append('--since=' + git_since)
+    if git_until is not None:
+        command.append('--until=' + git_until)
+    raw_git_log = subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE
+    )
+    if sys.version_info < (3, 0):
+        return raw_git_log.stdout
+    else:
+        return raw_git_log.stdout.read().decode('utf-8', 'ignore')
+
+
+if __name__ == "__main__":
+   main()
